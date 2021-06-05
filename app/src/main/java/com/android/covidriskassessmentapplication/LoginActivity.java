b817e188519ad7335a519b826842020d9318d81d
rajanvishwa10 <rajanvishwa10@gmail.com>
2021-06-05 21:52:48 +0530

    Keep logged in user

--- app/src/main/java/com/android/covidriskassessmentapplication/LoginActivity.java
+++ app/src/main/java/com/android/covidriskassessmentapplication/LoginActivity.java
@@ -7,9 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
 
     EditText editText;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth= FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser()!=null){
+            Intent intent=new Intent(this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
